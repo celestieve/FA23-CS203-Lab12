a5da34e2c5b20873f2166cd61a753e3a242604ea
celestieve <150277410+celestieve@users.noreply.github.com>
2023-11-14 19:42:12 -0600

    Add files via upload

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -1,6 +1,7 @@
-import java.io.File;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Encrypter {
@@ -34,8 +35,25 @@ public class Encrypter {
      */
     public void encrypt(String inputFilePath, String encryptedFilePath) throws Exception {
         //TODO: Call the read method, encrypt the file contents, and then write to new file
+    	String mssg = readFile(inputFilePath);
+    	encrypted = encrypt(mssg);
+    	writeFile(encrypted, encryptedFilePath);
     }
-
+    
+    private String encrypt(String mssg) {
+    	StringBuilder encryptedMssg = new StringBuilder();
+    	mssg = mssg.toLowerCase();
+    	
+    	for(int i = 0; i < mssg.length(); i ++) {
+    		char c = mssg.charAt(i);
+    		if (Character.isLetter(c)) {
+    			c = (char) ((c-'a' + shift + 26 )% 26 + 'a');
+    			
+    		}
+    		encryptedMssg.append(c);
+    	}
+		return encryptedMssg.toString();
+	}
     /**
      * Decrypts the content of an encrypted file and writes the result to another file.
      *
@@ -45,8 +63,25 @@ public class Encrypter {
      */
     public void decrypt(String messageFilePath, String decryptedFilePath) throws Exception {
         //TODO: Call the read method, decrypt the file contents, and then write to new file
+    	String encryptedMssg = readFile(messageFilePath);
+    	String decryptedMssg = decrypt(encryptedMssg);
+    	writeFile(decryptedMssg, decryptedFilePath);
     }
-
+    private String decrypt(String encryptedMssg) {
+		// TODO Auto-generated method stub
+    	StringBuilder decryptedMssg = new StringBuilder();
+    	encryptedMssg = encryptedMssg.toLowerCase();
+    	
+    	for(int i = 0; i < encryptedMssg.length(); i ++) {
+    		char c = encryptedMssg.charAt(i);
+    		if (Character.isLetter(c)) {
+    			c = (char) ((c -'a' - shift + 26 )% 26 + 'a');
+    			
+    		}
+    		decryptedMssg.append(c);
+    	}
+		return decryptedMssg.toString();
+	}
     /**
      * Reads the content of a file and returns it as a string.
      *
@@ -55,9 +90,19 @@ public class Encrypter {
      * @throws Exception if an error occurs while reading the file
      */
     private static String readFile(String filePath) throws Exception {
-        String message = "";
+    	StringBuilder message = new StringBuilder();
         //TODO: Read file from filePath
-        return message;
+    	try (Scanner input = new Scanner(Paths.get(filePath))) {
+        	while (input.hasNextLine()) {
+        		String line = input.nextLine();
+        		message.append(line).append("\n");
+        	}
+        	input.close();
+        }catch(Exception e) {
+        	System.out.println("error: " + e.toString());
+        	throw e;
+        }
+		return message.toString();
     }
 
     /**
@@ -68,6 +113,14 @@ public class Encrypter {
      */
     private static void writeFile(String data, String filePath) {
         //TODO: Write to filePath
+    	try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+    		writer.write(data);
+    		System.out.println("encrypted data written to: " + filePath);
+    	}catch (IOException e) {
+    		System.out.println("error" + filePath);
+    		e.printStackTrace();
+    		
+    	}
     }
 
     /**
